22e0366f510d9ea7ad79f3b2c957888dab93110a
Robert Glaser <robertbglaser@gmail.com>
2019-03-05 13:17:50 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("script.js")
+
+function truncateIt (word) {
+    
+    var prefix = "=? ";
+    var suffix = " ;"
+        
+    var lengthOfWord = word.length;
+    var firstLetter = word.charAt(0);
+    var lastLetter = word.charAt(lengthOfWord -1);
+    var truncatedWord = lengthOfWord - 2 
+    var returnWord = prefix + firstLetter + truncatedWord + lastLetter + suffix;
+
+    console.log(returnWord)
+    }
+
+    truncateIt("function") 
